1f2816ae796ea222c490c31fc66d9f63dc2540cd
KaiIOB <Kaibrendel@kabelmail.de>
2025-03-20 18:32:17 +0100

    additional logging if data request fails

--- wiim.js
+++ wiim.js
@@ -229,7 +229,10 @@ async function getWiimData(mywiimadapter, reqtype, ServName, IP_Address) {
                     mywiimadapter.setState(`${ServName}.sampleRate`, json.metaData.sampleRate, true);
                     mywiimadapter.setState(`${ServName}.bitDepth`, json.metaData.bitDepth, true);
                 } catch (error) {
-                    mywiimadapter.log.info(`something went wrong for ${ServName}:${error.message}`);
+                    mywiimadapter.log.info(`something went wrong for `+ ServName + `(` + IP_Address + `):` + error.message);
+                    mywiimadapter.log.info(`The request sent to the server was: ` + url );
+                    mywiimadapter.log.info(`The response was: ` + body);
+
                 }
             });
         }).on('error', error => {
